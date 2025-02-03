96cc141d996dcc40a0edbac3321660b05eee2379
Vishaldonda <vishal.d19@iiits.in>
2025-02-03 22:53:09 +0530

    added the edit and delete feature of typescript project

--- typescriptex/my-app/src/structure.ts
+++ typescriptex/my-app/src/structure.ts
@@ -36,8 +36,8 @@ export async function setup(element: HTMLElement) {
                 cell2.innerHTML = arrData[i]["name"];
                 cell3.innerHTML = arrData[i]["age"].toString();
                 cell4.innerHTML = arrData[i]["grade"];
-                cell5.innerHTML = '<button class="editButton" onclick="editButton(' + i + ')">Edit</button>'
-                cell6.innerHTML = '<button class="deleteButton" onclick="deleteButton(' + arrData[i]["id"] + ')">Delete</button>'
+                cell5.innerHTML = '<button class= "editButton"  onclick="editButton(this)"  tuple-id=' + arrData[i]["id"] + ' >Edit</button>'
+                cell6.innerHTML = '<button class="deleteButton" onclick="deleteButton(this)" tuple-id=' + arrData[i]["id"] + '>Delete</button>'
             }
         }
     }
@@ -102,14 +102,11 @@ export async function setup(element: HTMLElement) {
 
     const chartDataPromise = axios.get<Student[]>(endpointURL);
     const tableDataPromise = axios.get<Student[]>(endpointURL);
-    
+
     const [chartData, tableData] = await Promise.all([chartDataPromise, tableDataPromise]);
-    
-    // console.log("c1",chartDataPromise);
-    // console.log("c2",tableData);
 
     async function fetchJsonAPIDataTable() {
-        var responseJsonResult : Student[] = tableData.data
+        var responseJsonResult: Student[] = tableData.data
         console.log("Display Data stored: ", responseJsonResult)
         fetchTable(responseJsonResult);
     }
@@ -138,7 +135,7 @@ export async function setup(element: HTMLElement) {
         //----------- zod test
         const Schemanames = z.array(z.string());
         const Schemamarks = z.array(z.number());
-                
+
         Schemanames.parse(names); // => passes if data is strings
         Schemamarks.parse(mark); // => passes if data is numbers
         //-------------------------------------------------------
@@ -180,5 +177,114 @@ export async function setup(element: HTMLElement) {
 
     fetchJsonAPIDataTable()
     buildChart()
+
+
+    // ------edit button 
+
+    const editButtons = document.getElementsByClassName('editButton')
+
+    for (var i = 0; i < editButtons.length; i++) {
+        // console.log("here", editButtons[i]);
+        let tupleID = editButtons[i].getAttribute("tuple-id") as string;
+        editButtons[i]?.addEventListener('click', () => editButton(tupleID));
+    }
+
+
+    const deleteButtons = document.getElementsByClassName('deleteButton')
+
+    for (var i = 0; i < deleteButtons.length; i++) {
+        // console.log("here", deleteButtons[i]);
+        let tupleID = deleteButtons[i].getAttribute("tuple-id") as string;
+        deleteButtons[i]?.addEventListener('click', () => deleteButton(tupleID));
+    }
+
+
+    async function editButton(tid: string) {
+        let tupid: number = Number(tid)
+        console.log("edit function clicked", tupid);
+
+        const tableElement = document.getElementById("Table") as HTMLElement | null;
+        const addElement = document.getElementById("ADD") as HTMLElement | null;
+        const addtab = document.getElementById("add-tab") as HTMLElement | null;
+        // const tabletab = document.getElementById("table-tab") as HTMLElement | null;
+        const edittab = document.getElementById("edit-tab") as HTMLElement | null;
+        const editElement = document.getElementById("Edit") as HTMLElement | null;
+        const tupleElement = document.getElementById("tuple-number") as HTMLElement | null;
+
+
+
+        tupleElement!.style.display = "none"; // show us the id
+        // if (tableElement && addElement && editElement) {
+        addtab!.style.display = "none";
+        // tabletab!.style.display = "none";
+        edittab!.style.display = "block";
+        addElement!.style.display = "none";
+        tableElement!.style.display = "none";
+        editElement!.style.display = "block";
+        // } else {
+        // console.error("Not found");
+
+        //fetch data according to id
+
+        async function fetchdata() {
+            let endpointURL = "http://localhost:3000/students/marks";
+            let respjson = await axios.get(endpointURL);
+            console.log("get data", respjson.data);
+            return respjson.data
+        }
+
+        let resp: any = await fetchdata() // fetched the response
+
+        console.log("resp", resp, "tupid", tupid)
+        console.log(resp[tupid])
+
+        // let currresp = resp[tupid]
+        let currresp;
+        for (let i = 0; i < resp.length; i++) {
+            if (resp[i].id === tupid) {
+                currresp = resp[i];
+                break;
+            }
+        }
+
+        var nameV = currresp["name"]
+        var ageV = currresp["age"]
+        var gradeV = currresp["grade"]
+        //var markV = currresp["mark"]
+
+        var tuplenumber: any = document.getElementById('tuple-number') as HTMLElement;
+        tuplenumber.value = tupid
+        var name: any = document.getElementById('edit-name') as HTMLElement;
+        name.value = nameV
+        var age: any = document.getElementById('edit-age') as HTMLElement;
+        age.value = ageV
+        var grade: any = document.getElementById('edit-grade') as HTMLElement;
+        grade.value = gradeV
+
+    }
+
+    async function deleteButton(tid: string) {
+        let tupid: number = Number(tid)
+        console.log("delete function clicked (id)", tupid);
+
+        let endpointURL = "http://localhost:3000/students";
+        var idval = tupid;
+
+        let newEndPointURL = endpointURL + '/' + idval + ''
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const response = await fetch(newEndPointURL, {
+            method: "DELETE",
+            headers: myHeaders,
+        });
+
+        if (response.ok) {
+            document.getElementById("table-body")!.innerHTML = "";
+            setup(document.querySelector("#chart")!);
+        }
+        else {
+            console.log("Delete Record Unsucessfull");
+        }
+    }
 
 }
